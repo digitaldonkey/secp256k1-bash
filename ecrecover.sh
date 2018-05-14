3c92d480798edb640d62391916dc78aa56751893
Thorsten Krug <email@donkeymedia.eu>
2018-05-14 16:46:07 -0400

    Fix workdir and update.

--- ecrecover.sh
+++ ecrecover.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# Get the real path or working directory.
+# https://stackoverflow.com/a/246128/308533
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
 $(which node) ./runSecp265k1.js "$@"
